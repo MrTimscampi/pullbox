5a233ccb636242e487a26a5cdf8e49c800d64a78
MrTimscampi <julien.machiels@protonmail.com>
2018-05-03 15:24:13 +0200

    refactor: remove unused views and routes
    A few views and their routes added by the blueprint are useless for Pullbox.

--- pullbox/public/views.py
+++ pullbox/public/views.py
@@ -41,23 +41,3 @@ def logout():
     logout_user()
     flash('You are logged out.', 'info')
     return redirect(url_for('public.home'))
-
-
-@blueprint.route('/register/', methods=['GET', 'POST'])
-def register():
-    """Register new user."""
-    form = RegisterForm(request.form)
-    if form.validate_on_submit():
-        User.create(username=form.username.data, email=form.email.data, password=form.password.data)
-        flash('Thank you for registering. You can now log in.', 'success')
-        return redirect(url_for('public.home'))
-    else:
-        flash_errors(form)
-    return render_template('public/register.html', form=form)
-
-
-@blueprint.route('/about/')
-def about():
-    """About page."""
-    form = LoginForm(request.form)
-    return render_template('public/about.html', form=form)
